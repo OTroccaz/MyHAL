c5821f6b1c54028cdbf1c4dc098fbf9adc35cc11
Olivier Troccaz <olivier.troccaz@univ-rennes1.fr>
2021-05-26 09:26:31 +0200

    Update MyHAL.php

--- MyHAL.php
+++ MyHAL.php
@@ -687,7 +687,7 @@ if (isset($_POST["soumis"])) {
 																				<p class="font-italic">Photo : Viaduct France by Elizabeth Jamieson on Unsplash (détail)</p>
 
                                         <p class=" mb-2 text-justify">
-                                           MyHAL is a PHP program to help authors check their publication list in HAL, made by by Olivier Troccaz (design & coding) and Laurent Jonchère (design). If you need help, please contact <a target="_blank" href="https://openaccess.univ-rennes1.fr/interlocuteurs/laurent-jonchere">Laurent Jonchère</a> or <a target="_blank" href="https://ecobio.univ-rennes1.fr/personnel.php?qui=Olivier_Troccaz">Olivier Troccaz</a>.
+                                           MyHAL is a PHP program to help authors check their publication list in HAL, made by by Olivier Troccaz (design & coding) and Laurent Jonchère (design). If you need help, please contact <a target="_blank" href="https://openaccess.univ-rennes1.fr/interlocuteurs/laurent-jonchere">Laurent Jonchère</a> or <a target="_blank" href="https://openaccess.univ-rennes1.fr/interlocuteurs/olivier-troccaz">Olivier Troccaz</a>.
                                         </p>
 
 
