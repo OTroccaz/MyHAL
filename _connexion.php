f462b15007515abf2cdf179210569bf2c8d93667
Olivier Troccaz <olivier.troccaz@univ-rennes1.fr>
2023-07-07 09:49:53 +0200

    Authentification CAS (fin)

--- _connexion.php
+++ _connexion.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Zip2HAL - Importez vos publications dans HAL - Import your publications into HAL
+ * MyHAL - Vérifiez votre liste de publications - Check your paper list
  *
  * Copyright (C) 2023 Olivier Troccaz (olivier.troccaz@cnrs.fr) and Laurent Jonchère (laurent.jonchere@univ-rennes.fr)
  * Released under the terms and conditions of the GNU General Public License (https://www.gnu.org/licenses/gpl-3.0.txt)
